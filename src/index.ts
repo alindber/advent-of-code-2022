c177821dd76347f8a0ee3a6da8b66387492f9528
Andy Lindberg <andy.lindberg@shopify.com>
2022-12-05 22:38:01 -0700

    Day 6 - part 1 & 2

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,16 @@ import { day2 } from "./day2";
 import { day3 } from "./day3";
 import { day4 } from "./day4";
 import { day5 } from "./day5";
+import { day6 } from "./day6";
 
 day1();
+console.log();
 day2();
+console.log();
 day3();
+console.log();
 day4();
+console.log();
 day5();
+console.log();
+day6();
